e727aade327395f45f5b4a30330d1eebf10b5969
Qiushan_Linux <zhao940089273@163.com>
2021-05-30 18:00:46 +0800

    May 30

--- Coding_interviews/Q55/Q55sub2.cc
+++ Coding_interviews/Q55/Q55sub2.cc
@@ -5,10 +5,33 @@ class Solution {
 public:
     bool isBalanced(TreeNode* root) {
         if (!root) {return true;}
-        
+        return isBalancedCore(root) != -1;
+    }
+
+    int isBalancedCore(TreeNode* root) {
+        if (!root) {return 0;}
+        int left = isBalancedCore(root->left);
+        if (left == -1) {return -1;}
+        int right = isBalancedCore(root->right);
+        if (right == -1) {return -1;}
+        return std::abs(left - right) < 2 ? std::max(left, right) + 1 : -1;
     }
 };
 
 int main(int argc, const char** argv) {
+    Solution s;
+    // !root, only root, and only root with a left child or right child
+    // isBalanced Tree/ is not balaced tree, all nodes have no left/right children included
+    TreeNode* p8 = new TreeNode(8);
+    TreeNode* p6 = new TreeNode(6);
+    TreeNode* p4 = new TreeNode(4);
+    TreeNode* p2 = new TreeNode(2);
+    TreeNode* p7 = new TreeNode(7, p6, p8);
+    TreeNode* p3 = new TreeNode(3, p2, p4);
+    TreeNode* p5 = new TreeNode(5, p3, p7);
+    TreeNode* root = p5;
+    bool isBalanced = s.isBalanced(root);
+    if (isBalanced) {printf("This tree is a balanced tree\n");}
+    else {printf("This tree is not a balanced tree\n");}
     return 0;
 } 
---
+++ Coding_interviews/Q56/Q56.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+class Solution {
+public:
+    std::pair<int, int> findNumsAppearOnlyOnce(std::vector<int>& arr) {
+        if (arr.size() <= 1) {return {};}
+        int xorRes = 0;
+        for (const int& val : arr) {xorRes ^= val;}
+        if (xorRes == 0) {
+            printf("In array no numbers appear only once\n");
+            return {};
+        }
+        int findBit = 1;
+        while ((xorRes & findBit) == 0) {findBit << 1;}
+        int res1 = 0;
+        int res2 = 0;
+        for (const int& val : arr) {
+            // Here, you must be very very careful that '==' is advanced than bit operation!!!
+            if ((val & findBit) == 0) {res1 ^= val;}
+            if ((val & findBit) == 1) {res2 ^= val;}
+        }
+        return {res1, res2};
+    }
+};
+
+int main(int argc, const char** argv) {
+    Solution s;
+    // arr.size() <= 1, arr.size() == 2;
+    // array has no numbers appearing only once
+    // array has two numbers appearing only once`
+    std::vector<int> arr {1, 2, 10, 4, 1, 4, 2, 3};
+    std::pair<int, int> res = s.findNumsAppearOnlyOnce(arr);
+    printf("%d %d\n" ,res.first, res.second);
+    int a = 4 & 1;
+    printf("%d\n", a);
+    return 0;
+}
