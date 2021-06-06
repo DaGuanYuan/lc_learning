38bb63eb24a6b52b380976864c97cc9df8624e2a
Qiushan_Linux <zhao940089273@163.com>
2021-06-06 12:02:00 +0800

    DPandNonDPCatalog

--- Coding_interviews/Q66/Q66.cc
+++ Coding_interviews/Q66/Q66.cc
@@ -2,17 +2,16 @@
 #include <vector>
 
 class Solution {
+public:
     std::vector<int> multiply(std::vector<int>& a) {
-        if(a.size() == 0) return {};
-        std::vector<int> b (a.size(), 0);
-        b[0] = 1;
-        int tmp = 1;
-        for(int i = 1; i < a.size(); i++) {
-            b[i] = b[i - 1] * a[i - 1];
-        }
-        for(int i = a.size() - 2; i >= 0; i--) {
-            tmp *= a[i + 1];
-            b[i] *= tmp;
+        if (a.empty()) {return {};}
+        int n = a.size();
+        std::vector<int> b(n, 1);
+        for (int i = 1; i <= n - 1; i++) {b[i] = b[i - 1] * a[i - 1];}
+        int temp = 1;
+        for (int i = 2; i <= n; i++) {
+            temp *= a[n - i + 1];
+            b[n - i] *= temp;
         }
         return b;
     }
@@ -20,6 +19,19 @@ class Solution {
 
 int main(int argc, const char** argv) {
     Solution s;
+    // a is empty
+    // a has only one element
+    // a contains positive, negative and 0
+    std::vector<int> a {1, 2, 3, 4, 5};
+    std::vector<int> res = s.multiply(a);
+    if (res.empty()) {printf("The input is empty, Please have a check!\n");}
+    else {
+        printf("The mulitply array is:\n");
+        for (std::vector<int>::iterator it = res.begin(); it != res.end(); it++) {
+            printf("%d ", *it);
+        }
+        printf("\n");
+    } 
     return 0;
 
 }
