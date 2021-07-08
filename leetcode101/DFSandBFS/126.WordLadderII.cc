0dfbb352f4f8976d70f97f8baf024f64abeeda15
Qiushan_Linux <zhao940089273@163.com>
2021-07-08 21:31:16 +0800

    July 8

--- leetcode101/DFSandBFS/126.WordLadderII.cc
+++ leetcode101/DFSandBFS/126.WordLadderII.cc
@@ -1,30 +1,84 @@
 #include <iostream>
 #include <vector>
 #include <string.h>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
 
 using WordList = std::vector<std::string>;
 
 class Solution {
 public:
+    void getNeighbors(const std::string& str,
+                      std::unordered_map<std::string, WordList>& hashMap,
+                      const WordList& wordList,
+                      std::unordered_set<std::string>& st) {
+        if (st.count(str)) {st.erase(str);}
+        std::string change = str;
+        for (int i = 0; i < change.size(); ++i) {
+            change = str;
+            for (char ch = 'a'; ch <= 'z'; ++ch) {
+                change[i] = ch;
+                if (st.find(change) != st.end()) {hashMap[str].push_back(change);}
+            }
+        }
+        st.emplace(str);
+    }
+
     std::vector<WordList> findLadders(std::string beginWord,
                                       std::string endWord,
                                       WordList& wordList) {
         if (wordList.empty()) {return {};}
-        
+        std::vector<WordList> res;
+        std::unordered_map<std::string, WordList> hashMap;
+        std::unordered_set<std::string> st;
+        for (const std::string& str : wordList) {st.emplace(str);}
+        if (st.find(endWord) == st.end()) {return res;}
+
+        std::queue<WordList> q;
+        WordList current;
+        WordList needAdd;
+        int size = 0;
+        std::unordered_set<std::string> visited;
+        std::unordered_set<std::string> subVisited;
+        visited.emplace(beginWord);
+        bool endFlag = false;
+        q.push({beginWord});
+        while (!q.empty()) {
+            size = q.size();
+            subVisited.clear();
+            while (size--) {
+                current = q.front();
+                q.pop();
+                getNeighbors(current.back(), hashMap, wordList, st);
+                for (const std::string& str : hashMap[current.back()]) {
+                    if (visited.find(str) == visited.end()) {
+                        subVisited.emplace(str);
+                        needAdd = current;
+                        needAdd.push_back(str);
+                        q.push(needAdd);
+                    }
+                    if (str == endWord) {
+                        endFlag = true;
+                        res.push_back(needAdd);    
+                    }
+                }
+                hashMap[current.back()].clear();
+            }
+            for (const std::string& str : subVisited) {visited.emplace(str);}
+            if (endFlag) {break;}
+        }
+
+        return res;
     }
 };
 
-int main(int argc, const char** argv) {
-    std::vector<std::vector<int> > test;
-    std::vector<int> testSample1 {1, 2};
-    std::vector<int> testSample2 {1};
-    test.push_back(testSample1);
-    test.push_back(testSample2);
-    for (auto it1 = test.begin(); it1 != test.end(); ++it1) {
-        for (auto it2 = it1->begin(); it2 != it1->end(); ++it2) {
-            printf("%d ", *it2);
-        }
-        printf("\n");
-    }
-    return 0;
-}
+// int main(int argc, const char** argv) {
+//     std::unordered_map<int, WordList> hashMap;
+//     hashMap[0].push_back({"abc"});
+//     hashMap[0].push_back({"abc"});
+//     for (const std::string& str : hashMap[0]) {
+//         printf("%s\n", str.c_str());
+//     }
+//     return 0;
+// }
--- leetcode101/DFSandBFS/310.MinimumHeightTrees.cc
+++ leetcode101/DFSandBFS/310.MinimumHeightTrees.cc
@@ -1,10 +1,89 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 
 class Solution {
 public:
+    void findMinHeightTrees(std::vector<std::vector<int> >& graph,
+                            std::vector<int>& res,
+                            int& minDepth,
+                            int n,
+                            int operateNode) {
+        std::queue<int> q;
+        std::vector<bool> visited(n, false);
+        q.push(operateNode);
+        visited[operateNode] = true;
+        int depth = 0;
+        int size = 0;
+        int currentNode = 0;
+        while (!q.empty()) {
+            ++depth;
+            if (depth > minDepth) {return;}
+            size = q.size();
+            while (size--) {
+                currentNode = q.front();
+                q.pop();
+                for (const int& val : graph[currentNode]) {
+                    if (!visited[val]) {
+                        q.push(val);
+                        visited[val] = true;    
+                    }
+                }
+            }
+        }
+        if (depth < minDepth) {
+            res.clear();
+            minDepth = depth;
+        }
+        res.push_back(operateNode);
+    }
+
     std::vector<int> findMinHeightTrees(int n, std::vector<std::vector<int> >& edges) {
         if (n == 1) {return {0};}
-        
+        std::vector<std::vector<int> > graph(n);
+        for (const std::vector<int>& vec : edges) {
+            graph[vec[0]].push_back(vec[1]);
+            graph[vec[1]].push_back(vec[0]);
+        }
+        int minDepth = INT32_MAX;
+        std::vector<int> res;
+        for (int i = 0; i < n; ++i) {
+            findMinHeightTrees(graph, res, minDepth, n, i);
+        }
+        return res;
     }
+
+    std::vector<int> findMinHeightTrees_Optimal(int n, std::vector<std::vector<int> >& edges) {
+        if (n == 1) {return {0};}
+        std::vector<int> degrees(n);
+        std::vector<std::vector<int> > graph(n);
+        std::vector<int> res;
+        for (const std::vector<int>& vec : edges) {
+            ++degrees[vec[0]];
+            ++degrees[vec[1]];
+            graph[vec[0]].push_back(vec[1]);
+            graph[vec[1]].push_back(vec[0]);
+        }
+        std::queue<int> q;
+        for (int i = 0; i < n; ++i) {
+            if (degrees[i] == 1) {q.push(i);}
+        }
+        
+        int size = 0;
+        int current = 0;
+        while (!q.empty()) {
+            res.clear();
+            size = q.size();
+            while (size--) {
+                current = q.front();
+                q.pop();
+                res.push_back(current);
+                for (const int& val : graph[current]) {
+                    --degrees[val];
+                    if (degrees[val] == 1) {q.push(val);}
+                }
+            }
+        }
+        return res;
+    }   
 };
