113ca22dff9d447bfd7206ca93996181b43f88a6
Qiushan_Linux <zhao940089273@163.com>
2021-07-11 23:11:29 +0800

    July 11

--- leetcode101/ListNode/25.ReverseNodesink-Group.cc
+++ leetcode101/ListNode/25.ReverseNodesink-Group.cc
@@ -1,11 +1,10 @@
 #include <iostream>
-#include <tuple>
 #include "../../DataStructure/ListNode.h"
 
 class Solution {
 public:
-    std::pair<ListNode*, ListNode*> reverse(ListNode* head, ListNode* newHead) {
-        if (!head) {return {nullptr, nullptr};}
+    ListNode* reverse(ListNode* head, ListNode* newHead) {
+        if (!head) {return nullptr;}
         ListNode* pre = nullptr, *cur = head, *post = cur->pNext;
         while (post != newHead) {
             cur->pNext = pre;
@@ -14,30 +13,37 @@ public:
             post = post->pNext;
         }
         cur->pNext = pre;
-        return {cur, head};
+        return cur;
     }
 
     ListNode* reverseKGroup(ListNode* head, int k) {
         if (!head) {return head;}
-        int steps = k;
+        int steps = 0;
         ListNode* preHead = new ListNode(-1, head);
         ListNode* pre = preHead, *cur = head, *post = head->pNext, *newHead = cur;
-        while (steps--) {
-            newHead = newHead->pNext;
-            if (!newHead) {break;}
-        }
-        while (newHead) {
-            std::tie(cur, head) = reverse(head, newHead);
+        while (!steps) {
+            steps = k;
+            while (steps) {
+                newHead = newHead->pNext;
+                if (!newHead) {break;}
+                --steps;    //calculate steps after update newHead
+                // advantage: k == 0 || k == 1 || k > 1 indicates
+                // three different situations (wow, I'm so smart!)
+            }
+            if (steps) {break;}
+            cur = reverse(head, newHead);
             pre->pNext = cur;
             pre = head;
             head->pNext = newHead;
             head = newHead;
             cur = head;
             post = cur->pNext;
-            while (steps--) {
-                newHead = newHead->pNext;
-                if (!newHead) {break;}
-            }
+        }
+        if (steps == 1) {
+            cur = reverse(head, newHead);
+            pre->pNext = cur;
+            pre = head;
+            head->pNext = newHead;
         }
         return preHead->pNext;
     }
